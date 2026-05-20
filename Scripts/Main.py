90a9996fceedb63401b671c4e32921429b53e268
Rohit <rohitpathak987@gmail.com>
2026-05-20 15:14:42 +0530

    removed dataset commit

--- Scripts/Main.py
+++ Scripts/Main.py
@@ -4,12 +4,20 @@ from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 import torch
 import os
+from huggingface_hub import snapshot_download
 
 os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = "1"
 
 def main():
     device = "cuda" if torch.cuda.is_available() else "cpu"
     print(device)
+
+    # snapshot_download(
+    #     repo_id="RohitGENAICODER/ASVspoofLADataset",
+    #     repo_type="dataset",
+    #     local_dir="Datasets/LA",
+    #     token="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"
+    # )
 
     Data_root = r"D:\Deep Neural Network\ML-Audio_DeepFake\Datasets\LA"
 
@@ -41,9 +49,9 @@ def main():
         
     #     print(f"Count 0: {count_0}, Count 1 : {count_1}")
 
-    X,y = next(iter(train_dataloader))
-    print(X.shape)
-    print(y.shape)
+    # X,y = next(iter(train_dataloader))
+    # print(X.shape)
+    # print(y.shape)
 
 if __name__ == "__main__":
     main()

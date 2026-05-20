03351364cb4e49060fce1decc17b7b46373ff15d
Rohit <rohitpathak987@gmail.com>
2026-05-20 14:09:33 +0530

    Added Scripts
    Data to create mel spectograms and create labels.
    
    And main to create train and dev datasets

---
+++ Scripts/Main.py
@@ -0,0 +1,49 @@
+from datasets import load_dataset
+import data
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+import torch
+import os
+
+os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = "1"
+
+def main():
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print(device)
+
+    Data_root = r"D:\Deep Neural Network\ML-Audio_DeepFake\Datasets\LA"
+
+    #Instantiate Datasets
+    train_dataset = data.ASVpoofDataset(Data_root,part="train")
+    dev_dataset = data.ASVpoofDataset(Data_root, part="dev")
+
+    #Verify Train set labels
+    count_0 = train_dataset.file_labels.count(0)
+    count_1 = train_dataset.file_labels.count(1)
+    print(f"Count_0: {count_0}, Count_1: {count_1}")
+
+
+    # #Dataloader for train and dev dataset
+    # batch_size = 128
+    # train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    # dev_dataloader = DataLoader(dev_dataset, batch_size=batch_size, shuffle=False)
+
+    # #Testing out labels
+    # count_0 = 0
+    # count_1 = 0
+    # for i in range(1000):
+    #     _,label = train_dataloader.dataset[i]
+
+    #     if label.item() == 0:
+    #         count_0 +=1
+    #     else:
+    #         count_1 += 1
+        
+    #     print(f"Count 0: {count_0}, Count 1 : {count_1}")
+
+    X,y = next(iter(train_dataloader))
+    print(X.shape)
+    print(y.shape)
+
+if __name__ == "__main__":
+    main()
---
+++ Scripts/data.py
@@ -0,0 +1,70 @@
+import pandas as pd
+import librosa
+import os
+from torch.utils.data import Dataset
+import numpy as np
+import torch
+
+class ASVpoofDataset(Dataset):
+    def __init__(self, base_dir, part="train", sr=16000, n_mels=128):
+        super().__init__()
+
+        self.base_dir = base_dir
+        self.audio_dir = os.path.join(self.base_dir, f"ASVspoof2019_LA_{part}", "flac")
+        protocol_file = os.path.join(self.base_dir, "ASVspoof2019_LA_cm_protocols", 
+                                     f"ASVspoof2019.LA.cm.{part}.trl.txt")
+
+        self.sr = sr
+        self.n_mels = n_mels
+        self.label_map = {"bonafide":0, "spoof":1}
+
+        #Parse protocol to get file names and labels
+        self.file_list, self.file_labels = self._parse_protocol(protocol_file)
+
+    def _parse_protocol(self, protocol_file):
+        files,labels = [], []
+        with open(protocol_file, "r") as f:
+            for line in f:
+                parts = line.strip().split()
+
+                files.append(parts[1])
+                labels.append(self.label_map[parts[4].strip()])
+            
+        return files,labels
+    
+    def __len__(self):
+        return len(self.file_list)
+    
+    def __getitem__(self, index):
+        #Construct path and load data with librosa
+        audio_path = os.path.join(self.audio_dir, f"{self.file_list[index]}.flac")
+        y,_ = librosa.load(audio_path,sr=self.sr)
+
+        #Extract mel spectrogram using Librosa
+        mel_spec = librosa.feature.melspectrogram(y=y, sr=self.sr, n_mels=self.n_mels)
+
+        #Convert power to decibels
+        mel_decb = librosa.power_to_db(mel_spec,ref=np.max)
+        
+        #Adding padding for time frames to set it to 128
+        target_shape = 128
+        if mel_decb.shape[1] < target_shape:
+            pad_width = target_shape - mel_decb.shape[1]
+            mel_decb = np.pad(
+                mel_decb,
+                pad_width=((0,0), (0,pad_width)),
+                mode="constant"
+            )
+        else:
+            mel_decb = mel_decb[:, :target_shape]
+
+        #Add a channel dimensions for CNN
+        mel_decb = np.expand_dims(mel_decb,axis=0)
+
+        #Convert to pytorch labels
+        X_tensor = torch.tensor(mel_decb,dtype=torch.float32)
+        Y_tensor = torch.tensor(self.file_labels[index], dtype=torch.float32)
+
+        return X_tensor, Y_tensor
+
+
